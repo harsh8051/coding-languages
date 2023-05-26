88f39105cdd820169a7860954c611b1c878a8acd
harsh8051 <105968770+harsh8051@users.noreply.github.com>
2023-05-26 22:22:13 +0530

    some test on go main

--- GO/main.go
+++ GO/main.go
@@ -1,7 +1,29 @@
 package main
 
 import "fmt"
+import "os"
+import "log"
+import "flag"
 
+var (
+    home   = os.Getenv("HOME")
+    user   = os.Getenv("USER")
+    gopath = os.Getenv("GOPATH")
+)
+func init() {
+    if user == "" {
+        log.Fatal("$USER not set")
+    }
+    if home == "" {
+        home = "/home/" + user
+    }
+    if gopath == "" {
+        gopath = home + "/go"
+    }
+    // gopath may be overridden by --gopath flag on command line.
+    flag.StringVar(&gopath, "gopath", gopath, "override default GOPATH")
+}
 func main() {
+	
 	fmt.Println("Hello, 世界 Harshal How can I help you")
 }
